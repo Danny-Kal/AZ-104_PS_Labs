f93c90b284639cd5d1ef395c541d46947a12e487
Danny <dannykaladeen@gmail.com>
2023-04-11 01:07:12 -0600

    Find and change usage location

--- 01/Manage_AAD_Identities.ps1
+++ 01/Manage_AAD_Identities.ps1
@@ -1,1 +1,18 @@
-test for ps
+#Check Graph is installed
+Get-InstalledModule Microsoft.Graph
+
+#Check permissions needed for Graph cmdlet
+Find-MgGraphCommand -command Get-MgUser | Select -First 1 -ExpandProperty Permissions
+
+#Authenticate to Graph API
+$tenantid = "2e2d8b2f-2d7f-4c12-9bb7-90152233ddc5"
+Connect-MgGraph –TenantId $tenantid -Scopes "User.Read.All", "Group.ReadWrite.All"
+
+Get-MgUser
+$userId = f074369b-e7ef-4ff3-86c4-b12dc26c7044
+
+#Retrieve usage location of user
+get-mguser -userid $userId -property usagelocation | Select-Object usagelocation
+
+#Update usage location of user
+update-mguser -userid $userId -usagelocation gb
