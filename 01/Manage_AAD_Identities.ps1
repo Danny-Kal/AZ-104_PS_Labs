d5c8785b410b48b5eab58b99e9b1a45703268eed
Danny <dannykaladeen@gmail.com>
2023-04-12 22:54:54 -0600

    Listing AAD roles

--- 01/Manage_AAD_Identities.ps1
+++ 01/Manage_AAD_Identities.ps1
@@ -18,3 +18,18 @@ get-mguser -userid $userId -property usagelocation | Select-Object usagelocation
 #Update usage location of user
 update-mguser -userid $userId -usagelocation gb
 
+#Assign password to secure variable and create user
+$PasswordProfile = @{
+  Password = 'xWwvJ]6NMw+bWH-d'
+  }
+New-MgUser -DisplayName 'az104-01a-aaduser1' -PasswordProfile $PasswordProfile -AccountEnabled -MailNickName 'az104-01a-aaduser1' -UserPrincipalName 'az104-01a-aaduser1@contoso.com'
+
+#Find the ID for the user
+Get-MgUser
+
+#List the Azure AD roles of the user
+$uri = "https://graph.microsoft.com/beta/roleManagement/directory/transitiveRoleAssignments?`$count=true&`$filter=principalId eq 'xxxxx-xxxxx-xxxxx-xxxxx-xxxxx'"
+$method = 'GET'
+$headers = @{'ConsistencyLevel' = 'eventual'}
+(Invoke-MgGraphRequest -Uri $uri -Headers $headers -Method $method -Body $null).value
+
