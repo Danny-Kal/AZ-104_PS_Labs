ecb556f139d78114bf72a89f777e8ea59c9d0335
Danny <dannykaladeen@gmail.com>
2023-04-13 18:57:51 -0600

    Working with AzureAD roles

--- 01/Manage_AAD_Identities.ps1
+++ 01/Manage_AAD_Identities.ps1
@@ -27,9 +27,18 @@ New-MgUser -DisplayName 'az104-01a-aaduser1' -PasswordProfile $PasswordProfile -
 #Find the ID for the user
 Get-MgUser
 
+      # An alternative getting AAD roles of a user with Graph. Just get the members of a specific AzureAD role instead. One that the user in question would be a part of.
+      # $RoleMembers = get-mgdirectoryrolemember -directoryroleid 869b5bab-610e-4f2f-94a4-515a323da26f
+      # Then something here like get-MgUser where UserId eq $RoleMembers
+
 #List the Azure AD roles of the user
 $uri = "https://graph.microsoft.com/beta/roleManagement/directory/transitiveRoleAssignments?`$count=true&`$filter=principalId eq 'xxxxx-xxxxx-xxxxx-xxxxx-xxxxx'"
 $method = 'GET'
 $headers = @{'ConsistencyLevel' = 'eventual'}
 (Invoke-MgGraphRequest -Uri $uri -Headers $headers -Method $method -Body $null).value
+
+#Find the name associated with the roleID provided in previous step
+Import-Module Microsoft.Graph.Identity.DirectoryManagement
+Get-MgDirectoryRole | where-object RoleTemplateId -eq "62e90394-69f5-4237-9190-012177145e10" | select-object DisplayName,RoleTemplateId
+#Get-MgDirectoryRole -property DisplayName,RoleTemplateId | where-object RoleTemplateId -eq "62e90394-69f5-4237-9190-012177145e10" | fl
 
